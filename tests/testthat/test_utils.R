263084d4ca7e0e460ca375831e85e0782b7aabb5
Michael Mayer <mayermichael79@gmail.com>
2023-06-28 18:51:38 +0200

    finished unit tests for utils.R

--- tests/testthat/test_utils.R
+++ tests/testthat/test_utils.R
@@ -199,16 +199,78 @@ test_that("basic_checks fire some errors", {
   expect_error(basic_check(X = iris, v = "Species", pred_fun = predict, w = 1:3))
 })
 
+test_that("postprocess() works", {
+  expect_error(postprocess(num = 1:3))
+  num <- cbind(a = 1:3, b = c(1, 1, 1))
+  denom <- cbind(a = 1:3, b = 1:3)
+  
+  expect_equal(postprocess(num = num), num[3:1, ])
+  expect_equal(postprocess(num = num, denom = denom), num / denom)
+  expect_equal(postprocess(num = num, sort = FALSE), num)
+  expect_equal(postprocess(num = num, sort = FALSE, top_m = 2), num[1:2, ])
+  expect_equal(postprocess(num = num, squared = FALSE), sqrt(num[3:1, ]))
+})
+
+postprocess <- function(num, denom = 1, normalize = TRUE, squared = TRUE, 
+                        sort = TRUE, top_m = Inf, eps = 1e-8) {
+  out <- .zap_small(num, eps = eps)
+  if (normalize) {
+    out <- out / denom
+  }
+  if (!squared) {
+    out <- sqrt(out)
+  }
+  if (sort) {
+    out <- out[order(-rowSums(out)), , drop = FALSE]
+  }
+  utils::head(out, n = top_m)
+}
+
 test_that(".zap_small() works for vector input", {
   expect_equal(.zap_small(1:3), 1:3)
   expect_equal(.zap_small(c(1:3, NA)), c(1:3, 0))
   expect_equal(.zap_small(c(0.001, 1), eps = 0.01), c(0, 1))
 })
 
-test_that(".zap_small() works for matrix input", {
-  X <- cbind(a = 1:3, b = 3:1)
-  expect_equal(.zap_small(X), X)
-  expect_equal(.zap_small(rbind(X, c(NA, NA))), rbind(X, c(0, 0)))
-  expect_equal(.zap_small(cbind(0.001, 1), eps = 0.01), cbind(0, 1))
+test_that("poor_man_stack() works (test could be improved", {
+  y <- c("a", "b", "c")
+  z <- c("aa", "bb", "cc")
+  X <- data.frame(x = 1:3, y = y, z = z)
+  out <- poor_man_stack(X, to_stack = c("y", "z"))
+  xpected <- data.frame(
+    x = rep(1:3, times = 2L), 
+    varying_ = rep(c("y", "z"), each = 3L),
+    value_ = c(y, z)
+  )
+  expect_equal(out, xpected)
+  
+  expect_error(poor_man_stack(cbind(a = 1:3, b = 2:4), to_stack = "b"))
 })
 
+test_that("mat2df() works (test could be improved)", {
+  mat <- cbind(y = 1:2, z = c(0.5, 0.5))
+  rownames(mat) <- letters[seq_len(nrow(mat))]
+  out <- mat2df(mat)
+  rownames(out) <- NULL
+  xpected <- data.frame(
+    id_ = "Overall", 
+    variable_ = factor(c("a", "b", "a", "b"), levels = c("b", "a")),
+    varying_ = c("y", "y", "z", "z"),
+    value_ = c(1, 2, 0.5, 0.5),
+    stringsAsFactors = FALSE
+  )
+  expect_equal(out, xpected)
+  
+  mat_no_names <- mat
+  colnames(mat_no_names) <- NULL
+  expect_equal(unique(mat2df(mat_no_names)$varying_), c("y1", "y2"))
+  
+  expect_error(mat2df(head(iris)))
+  expect_error(mat2df(1:4))
+})
+
+test_that("qcut() works (test should be improved)", {
+  x <- 1:100
+  expect_equal(levels(qcut(x, m = 2)), c("[1,50]", "(50,100]"))
+  expect_equal(levels(qcut(x, m = 4)), c("[1,25]", "(25,50]", "(50,75]", "(75,100]"))
+})
