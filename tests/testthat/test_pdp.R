423377b83b4efc9e8bc202ec6cd5eab692542be8
Michael Mayer <mayermichael79@gmail.com>
2023-06-30 21:16:26 +0200

    Tests for PDP()

--- tests/testthat/test_pdp.R
+++ tests/testthat/test_pdp.R
@@ -81,3 +81,170 @@ test_that("pd_raw() also works for multioutput situations", {
   pd <- pd_raw(fit3, v = "Petal.Width", X = iris, grid = 2:3)
   expect_equal(dim(pd), c(2L, 2L))
 })
+
+# Now, PDP()
+fit1 <- lm(Sepal.Length ~ ., data = iris)
+fit2 <- lm(as.matrix(iris[1:2]) ~ Petal.Length + Petal.Width * Species, data = iris)
+
+test_that("PDP() returns a ggplot in different situations", {
+  expect_s3_class(PDP(fit1, v = "Petal.Width", X = iris), "ggplot")
+  expect_s3_class(PDP(fit2, v = "Petal.Width", X = iris), "ggplot")
+  
+  # With discrete "by"
+  expect_s3_class(PDP(fit1, v = "Petal.Width", X = iris, BY = "Species"), "ggplot")
+  expect_s3_class(PDP(fit1, v = "Petal.Width", X = iris, BY = "Species"), "ggplot")
+  
+  # With continuous "by"
+  expect_s3_class(PDP(fit1, v = "Petal.Width", X = iris, BY = "Sepal.Width"), "ggplot")
+  expect_s3_class(PDP(fit1, v = "Petal.Width", X = iris, BY = "Sepal.Width"), "ggplot")
+})
+
+test_that("PDP() does not plot in multivariable case", {
+  expect_error(PDP(fit1, v = c("Petal.Width", "Species"), X = iris, plot = TRUE))
+})
+
+test_that("PDP() returns the same values as pd_raw()", {
+  g <- rev(univariate_grid(iris$Species))
+  pd1 <- pd_raw(fit1, v = "Species", X = iris, grid = g)
+  pd2 <- PDP(fit1, v = "Species", X = iris, plot = FALSE, grid = g)
+  expect_equal(cbind.data.frame(Species = g, y = pd1), pd2)
+  
+  pd1 <- pd_raw(fit2, v = "Species", X = iris, grid = g)
+  pd2 <- PDP(fit2, v = "Species", X = iris, plot = FALSE, grid = g)
+  expect_equal(cbind.data.frame(Species = g, pd1), pd2)
+})
+
+test_that("PDP() reacts on grid order", {
+  g1 <- univariate_grid(iris$Species)
+  g2 <- rev(g1)
+  
+  pd1 <- PDP(fit1, v = "Species", X = iris, plot = FALSE, grid = g1)
+  pd2 <- PDP(fit1, v = "Species", X = iris, plot = FALSE, grid = g2)
+
+  rownames(pd2) <- 3:1
+  expect_equal(pd1, pd2[3:1, ])
+  
+  pd1 <- PDP(fit2, v = "Species", X = iris, plot = FALSE, grid = g1)
+  pd2 <- PDP(fit2, v = "Species", X = iris, plot = FALSE, grid = g2)
+  
+  rownames(pd2) <- 3:1
+  expect_equal(pd1, pd2[3:1, ])
+})
+
+test_that("PDP() with BY is same as stratified application", {
+  pd1 <- PDP(fit1, v = "Sepal.Width", X = iris, plot = FALSE, BY = "Species")
+  g <- unique(pd1$Sepal.Width)
+  ir <- split(iris, iris$Species)
+  pd2 <- rbind(
+    PDP(fit1, v = "Sepal.Width", X = ir[[1L]], plot = FALSE, grid = g),
+    PDP(fit1, v = "Sepal.Width", X = ir[[2L]], plot = FALSE, grid = g),
+    PDP(fit1, v = "Sepal.Width", X = ir[[3L]], plot = FALSE, grid = g)
+  )
+  pd2 <- data.frame(Species = rep(unique(iris$Species), each = length(g)), pd2)
+  expect_equal(pd1, pd2)
+  
+  # Multioutput
+  pd1 <- PDP(fit2, v = "Petal.Width", X = iris, plot = FALSE, BY = "Species")
+  g <- unique(pd1$Petal.Width)
+  ir <- split(iris, iris$Species)
+  pd2 <- rbind(
+    PDP(fit2, v = "Petal.Width", X = ir[[1L]], plot = FALSE, grid = g),
+    PDP(fit2, v = "Petal.Width", X = ir[[2L]], plot = FALSE, grid = g),
+    PDP(fit2, v = "Petal.Width", X = ir[[3L]], plot = FALSE, grid = g)
+  )
+  pd2 <- data.frame(Species = rep(unique(iris$Species), each = length(g)), pd2)
+  expect_equal(pd1, pd2)
+})
+
+test_that("PDP() does subsampling", {
+  set.seed(1L)
+  pd1 <- PDP(fit1, v = "Sepal.Width", X = iris, plot = FALSE, n_max = 10L)
+
+  set.seed(2L)
+  pd2 <- PDP(fit1, v = "Sepal.Width", X = iris, plot = FALSE, n_max = 10L)
+  
+  expect_false(identical(pd1, pd2))
+})
+
+test_that("PDP() reacts on grid strategy", {
+  pd1 <- PDP(
+    fit1, v = "Sepal.Width", X = iris, plot = FALSE, strategy = "uni", grid_size = 5L
+  )
+  pd2 <- PDP(
+    fit1, v = "Sepal.Width", X = iris, plot = FALSE, strategy = "quant", grid_size = 5L
+  )
+  expect_false(identical(pd1, pd2))
+})
+
+test_that("PDP() reacts on grid size", {
+  pd1 <- PDP(
+    fit1, v = "Sepal.Width", X = iris, plot = FALSE, strategy = "q", grid_size = 5L
+  )
+  pd2 <- PDP(
+    fit1, v = "Sepal.Width", X = iris, plot = FALSE, strategy = "q", grid_size = 10L
+  )
+  expect_false(identical(pd1, pd2))
+})
+
+test_that("PDP() reacts on grid", {
+  g <- 1:4
+  pd1 <- PDP(fit1, v = "Sepal.Width", X = iris, plot = FALSE, strategy = "q", grid = g)
+  pd2 <- PDP(fit1, v = "Sepal.Width", X = iris, plot = FALSE, strategy = "q")
+  expect_false(identical(pd1, pd2))
+})
+
+test_that("PDP() reacts on trim", {
+  pd1 <- PDP(
+    fit1, 
+    v = "Sepal.Width", 
+    X = iris, 
+    plot = FALSE, 
+    strategy = "q", 
+    trim = c(0.2, 0.8),
+    grid_size = 5L
+  )
+  pd2 <- PDP(
+    fit1, 
+    v = "Sepal.Width", 
+    X = iris, 
+    plot = FALSE, 
+    strategy = "q",
+    trim = 0:1, 
+    grid_size = 5L,
+  )
+  expect_false(identical(pd1, pd2))
+})
+
+test_that("PDP() does not react on constant weights", {
+  pd1 <- PDP(fit1, v = "Sepal.Width", X = iris, plot = FALSE)
+  pd2 <- PDP(fit1, v = "Sepal.Width", X = iris, plot = FALSE, w = rep(2, times = 150L))
+  expect_equal(pd1, pd2)
+})
+
+test_that("PDP() reacts on non-constant weights", {
+  pd1 <- PDP(fit1, v = "Sepal.Width", X = iris, plot = FALSE)
+  pd2 <- PDP(fit1, v = "Sepal.Width", X = iris, plot = FALSE, w = 1:150)
+  expect_false(identical(pd1, pd2))
+})
+
+test_that("PDP() works with vector BY or variable name BY", {
+  pd1 <- PDP(fit1, v = "Sepal.Width", X = iris, plot = FALSE, BY = "Species")
+  pd2 <- PDP(fit1, v = "Sepal.Width", X = iris, plot = FALSE, BY = iris$Species)
+  colnames(pd2)[1L] <- "Species"
+  expect_equal(pd1, pd2)
+})
+
+test_that("PDP() gives same answer on example as iml 0.11.1", {
+  # library(iml)
+  # mod <- Predictor$new(fit, data = iris)
+  # FeatureEffect$new(mod, feature = "Species", method = "pdp")$results
+  # FeatureEffect$new(mod, feature = "Sepal.Width", method = "pdp", grid.points = 2:4)$results
+  
+  iml_species <- c(6.847179, 5.737053, 5.260083)
+  pd1 <- PDP(fit, v = "Species", X = iris, plot = FALSE)
+  expect_equal(iml_species, pd1$y, tolerance = 0.001)
+  
+  iml_sw <- c(5.309279, 5.814375, 6.319470)
+  pd2 <- PDP(fit, v = "Sepal.Width", X = iris, grid = 2:4, plot = FALSE)
+  expect_equal(iml_sw, pd2$y, tolerance = 0.001)
+})
